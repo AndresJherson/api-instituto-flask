8a08e13e1d890de83ca4f9183e1eb242adb6e215
Andres <andresxmen2020@gmail.com>
2024-09-12 14:24:42 -0500

    commit 1

---
+++ src/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+from service.Conector import Conector
+
+app = Flask(__name__)
+conector = Conector()
+
+
+@app.route( '/productos', methods = [ 'GET' ] )
+def index():
+    data = conector.execute_query( "select * from productos" )
+    return jsonify( data )
+
+
+@app.route( '/productos', methods = [ 'POST' ] )
+def crear_producto():
+    nuevo_producto = request.json
+    nombre = nuevo_producto['nombre']
+    descripcion = nuevo_producto['descripcion']
+    precio = nuevo_producto['precio']
+    id = conector.get_id( 'productos' )
+    print( nuevo_producto )
+    print( id )
+
+    filas_afectadas = conector.execute_non_query( "INSERT INTO productos (id, nombre, descripcion, precio) VALUES (%s, %s, %s, %s)", (id, nombre, descripcion, precio) )
+
+    return jsonify( { "filas_afectadas": filas_afectadas } )
+
+
+@app.route( '/productos/<int:id>', methods = [ 'PUT' ] )
+def actualizar_producto( id ):
+    producto_actualizado = request.json
+    nombre = producto_actualizado['nombre']
+    descripcion = producto_actualizado['descripcion']
+    precio = producto_actualizado['precio']
+    
+    filas_afectadas = conector.execute_non_query("UPDATE productos SET nombre = %s, descripcion = %s, precio = %s WHERE id = %s", (nombre, descripcion, precio, id))
+    
+    return jsonify( { "filas_afectadas": filas_afectadas } )
+
+
+@app.route( '/productos/<int:id>', methods = [ 'DELETE' ] )
+def eliminar_producto( id ):
+    filas_afectadas = conector.execute_non_query("DELETE FROM productos WHERE id = %s", ( id, ))
+    
+    return jsonify( { "filas_afectadas": filas_afectadas } )
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/service/Conector.py
@@ -0,0 +1,70 @@
+import mysql.connector
+
+class Conector:
+    
+    conexion = None
+    cursor = None
+    
+    def __init__(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host='mysql8002.site4now.net',
+                user='aaca99_ventas',
+                password='ventas123',
+                database='db_aaca99_ventas',
+            )
+            if self.conexion.is_connected():
+                self.cursor = self.conexion.cursor()
+            else:
+                print("Error: No se pudo establecer la conexión a la base de datos.")
+            
+            # print( 'conector: ' * self.conexion )
+            # print( 'cursor: ' * self.cursor )
+        
+        except mysql.connector.Error as e:
+            print("Error de conexión: ", e)
+        
+    def execute_query(self, query: str):
+        try:
+            self.cursor.execute(query)
+            filas = self.cursor.fetchall()
+
+            data = []
+            for fila in filas:
+                data.append([x for x in fila])
+
+            return data
+        except mysql.connector.Error as e:
+            print("Error ejecutando la consulta: ", e)
+            return None
+    
+    def execute_non_query(self, query: str, parametros):
+        try:
+            self.cursor.execute(query, parametros)
+            self.conexion.commit()
+            filas_afectadas = self.cursor.rowcount
+            return filas_afectadas
+        
+        except mysql.connector.Error as e:
+            print("Error ejecutando la consulta: ", e)
+            return 0
+    
+    def get_id(self, tabla: str):
+        try:
+            query = f"SELECT MAX(id) FROM {tabla}"
+            self.cursor.execute(query)
+            fila = self.cursor.fetchone()
+
+            if fila and fila[0] is not None:
+                return fila[0] + 1
+            else:
+                return 1
+            
+        except mysql.connector.Error as e:
+            print("Error obteniendo el id: ", e)
+            return None
+        
+    def __del__(self):
+        if self.conexion.is_connected():
+            self.cursor.close()
+            self.conexion.close()
